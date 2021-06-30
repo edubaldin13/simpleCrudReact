af9940855a1f79597837e81f98838c6fb9689e3d
Eduardo Baldin <eduarbaldin@gmail.com>
2021-06-30 14:27:10 -0300

    Sending to the cloud

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>CRUD REACT</h1>
+<br>
+-> This is a CRUD React project, focused in inserting data into db.json 
+file, on frontend you can put your personal <strong>e-mail</strong> and your <strong>name</strong>.
+
+<h2>Starting</h2>
+
+-> On frontend terminal type "npm start" 
+-> On backend terminal type "npm start"
+-> Now just enjoy it!
+
---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "../template/Main";
+export default (props) => (
+  <Main icon="home" title="Home" subtitle="Este projeto, serve como exemplo para um sistema de cadastro.">
+    <div className="display-4">Bem Vindo!</div> <hr />
+    <p className="mb-0">
+      Sistema para exemplificar a construção de uim cadastro desenvolvido em
+      React!
+    </p>{" "}
+    {/* Adicionar a Logo do React aqui!*/}
+  </Main>
+);
