4e1b7ba7096d170ba25eeba665d18bc8b8cee9ab
Eduardo Baldin <eduarbaldin@gmail.com>
2021-11-03 21:49:50 -0300

    Sending it up

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -4,7 +4,7 @@ export default (props) => (
   <Main icon="home" title="Home" subtitle="Este projeto, serve como exemplo para um sistema de cadastro.">
     <div className="display-4">Bem Vindo!</div> <hr />
     <p className="mb-0">
-      Sistema para exemplificar a construção de uim cadastro desenvolvido em
+      Sistema para exemplificar a construção de um cadastro desenvolvido em
       React!
     </p>{" "}
     {/* Adicionar a Logo do React aqui!*/}
